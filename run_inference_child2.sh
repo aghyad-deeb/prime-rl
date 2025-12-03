14920911a8b93c85da0c5fa2acd098854ab86694
aghyad-deeb <th3elctronicag@gmail.com>
2025-12-02 22:58:16 -0800

    child2

---
+++ run_inference_child2.sh
@@ -0,0 +1,10 @@
+# On node 1
+uv run inference \
+	--model.name "Qwen/Qwen3-32b" \
+	--data-parallel-size 6 \
+	--tensor-parallel-size 4 \
+	--data-parallel-size-local 2 \
+	--data-parallel-address $DATA_PARALLEL_ADDRESS \
+	--data-parallel-rpc-port $DATA_PARALLEL_RPC_PORT \
+	--data-parallel-start-rank 4 \
+	--headless
