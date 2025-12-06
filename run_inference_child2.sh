0516268ca2eee7376510c475737634fb01705254
aghyad-deeb <th3elctronicag@gmail.com>
2025-12-05 21:50:49 -0800

    latest infer child2

--- run_inference_child2.sh
+++ run_inference_child2.sh
@@ -1,10 +1,10 @@
 uv run inference @ infer.toml \
 	--model.name "Qwen/Qwen3-32b" \
-	--data-parallel-size 6 \
-	--tensor-parallel-size 4 \
-	--data-parallel-size-local 2 \
+	--data-parallel-size 12 \
+	--tensor-parallel-size 2 \
+	--data-parallel-size-local 4 \
 	--data-parallel-address $DATA_PARALLEL_ADDRESS \
 	--data-parallel-rpc-port $DATA_PARALLEL_RPC_PORT \
-	--data-parallel-start-rank 4 \
+	--data-parallel-start-rank 8 \
 	--api-key $INFERENCE_SERVER_API_KEY \
 	--headless
