22084b7517b386bbb98673b9d829a26eb25056e9
aghyad-deeb <th3elctronicag@gmail.com>
2025-12-05 21:55:34 -0800

    latest orch and train bash

--- run_orch.sh
+++ run_orch.sh
@@ -1,5 +1,4 @@
 uv run orchestrator @ orch.toml \
-    --model.name "Qwen/Qwen3-32b" \
     --log.level debug \
     --client.base-url http://$INFERENCE_SERVER_IP:8000/v1 \
     --client.api-key-var INFERENCE_SERVER_API_KEY \
--- run_train.sh
+++ run_train.sh
@@ -2,6 +2,5 @@ uv run torchrun \
     --nproc-per-node 8 \
     --local-ranks-filter 0 \
     src/prime_rl/trainer/rl/train.py  @ trainer.toml \
-        --model.name "Qwen/Qwen3-32b" \
         --output-dir $OUTPUT_DIR \
         --log.level debug \
